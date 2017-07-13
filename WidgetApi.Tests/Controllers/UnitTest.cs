f6351038572063eb172592d6469222eaa937d5b1
bhavyams <v-bhavkr@microsoft.com>
2017-07-13 16:27:34 -0700

    initial commit

---
+++ WidgetApi.Tests/Controllers/UnitTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Results;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers.Tests
+{
+    [TestClass()]
+    public class TestHomeController
+    {
+        [TestMethod]
+        public void GetAllWidgets_ShouldReturnAllWidgets()
+        {
+            var testWidgets = GetTestWidgets();
+            var controller = new HomeController(testWidgets);
+
+            var result = controller.GetAll() as JsonResult<List<Widget>>;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Content);
+        }
+
+        [TestMethod]
+        public void GetWidget_ShouldReturnCorrectWidget()
+        {
+            var testWidgets = GetTestWidgets();
+            var controller = new HomeController(testWidgets);
+
+            var result = controller.Get(4) as JsonResult<Widget>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testWidgets[3].Name, result.Content.Name);
+        }
+
+        [TestMethod]
+        public void GetWidget_ShouldNotFindWidget()
+        {
+            var controller = new HomeController(GetTestWidgets());
+
+            var result = controller.Get(999);
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        private List<Widget> GetTestWidgets()
+        {
+            var testWidgets = new List<Widget>();
+            testWidgets.Add(new Widget { Id = 1, Name = "Demo1", Description = "Desc1", Price = 1 });
+            testWidgets.Add(new Widget { Id = 2, Name = "Demo2", Description = "Desc2", Price = 3.75 });
+            testWidgets.Add(new Widget { Id = 3, Name = "Demo3", Description = "Desc3", Price = 16.99 });
+            testWidgets.Add(new Widget { Id = 4, Name = "Demo4", Description = "Desc4", Price = 11.00 });
+
+            return testWidgets;
+        }
+    }
+}
---
+++ WidgetApi/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private List<Widget> widgets = new List<Widget>();
+        
+        public HomeController() { }
+
+        public HomeController(List<Widget> widgets)
+        {
+            this.widgets = widgets; 
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            return Json(widgets);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var w = widgets.Where(d => d.Id == id).FirstOrDefault();
+            if(w == null)
+            {
+                return NotFound();
+            }
+            return Json(w);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(Widget w)
+        {
+            widgets.Add(w);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(Widget w)
+        {
+            var widget = widgets.Where(d => d.Id == w.Id).FirstOrDefault();
+            if(widget == null)
+            {
+                return NotFound();
+            }
+            widget.Name = w.Name;
+            widget.Description = w.Description;
+            widget.Price = w.Price;
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var widget = widgets.Where(d => d.Id == id).FirstOrDefault();
+            if(widget == null)
+            {
+                return NotFound();
+            }
+            widgets.Remove(widget);
+            return Ok();
+        }
+    }
+}
